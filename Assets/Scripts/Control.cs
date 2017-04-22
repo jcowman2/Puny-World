9523edc5b2b12ac36ff9bf828fc748753897ba90
Joe Cowman <joe.cowman@huskers.unl.edu>
2017-04-22 17:44:16 -0500

    major build 2. dynamic request card

---
+++ Assets/Scripts/Control.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Control : MonoBehaviour {
+
+    public List<string> actionList;
+
+	void Start () {
+        actionList = new List<string>();
+	}
+	
+	void Update () {
+		
+	}
+
+    public void PostAction (GameObject btn) {
+
+        string label = btn.GetComponentInParent<PrayerCard>().label;
+        string outputCode = label + "_";
+
+        if (btn.CompareTag("Response")) {
+            outputCode += btn.GetComponentInChildren<Text>().text.Replace(" ", "");
+        } else if (btn.CompareTag("Random")) {
+            outputCode += "Random";
+        } else if (btn.CompareTag("Ignore")) {
+            outputCode += "Ignore";
+        } else if (btn.CompareTag("Smite")) {
+            outputCode += "Smite";
+        }
+
+        Debug.Log(outputCode);
+        actionList.Add(outputCode);
+
+    }
+}
---
+++ Assets/Scripts/OtherBtn.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OtherBtn : MonoBehaviour {
+
+    private Control gameControl;
+
+    private void Start () {
+        gameControl = GameObject.FindGameObjectWithTag("GameControl").GetComponent<Control>();
+    }
+
+
+	public void postAction() {
+        gameControl.PostAction(gameObject);
+    }
+}
