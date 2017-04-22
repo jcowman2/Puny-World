f320125e0c758f7cfc5bd705bf05a4f41d89fce1
Joe Cowman <joe.cowman@huskers.unl.edu>
2017-04-22 14:22:32 -0500

    rotating earth

---
+++ Assets/Scripts/SmoothRotate.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmoothRotate : MonoBehaviour {
+
+    public float secondsPerRotation;
+
+    private float elapsedTime;
+    private RectTransform rectTf;
+
+    private void Start () {
+        rectTf = gameObject.GetComponent<RectTransform>();
+        elapsedTime = 0;
+    }
+
+    private void Update () {
+        elapsedTime += Time.deltaTime;
+
+        if (elapsedTime >= secondsPerRotation) {
+            elapsedTime = elapsedTime % secondsPerRotation;
+        }
+
+        rectTf.rotation = Quaternion.AngleAxis(360 * (elapsedTime / secondsPerRotation), Vector3.forward);
+    }
+
+
+}
