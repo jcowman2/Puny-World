1b9ca413e761c4546665671a495bf99c45729d9e
Joe Cowman <joe.cowman@huskers.unl.edu>
2017-04-22 10:56:49 -0500

    vertical layout

---
+++ Assets/Scripts/PrayerCardCreator.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrayerCardCreator : MonoBehaviour {
+
+    public bool createCard;
+
+    public GameObject templateCard;
+
+    void CreateCard() {
+        Debug.Log("Creating Card");
+    }
+	
+	void Start () {
+		
+	}
+	
+	
+	void Update () {
+		
+        if (createCard) {
+            CreateCard();
+            createCard = false;
+        }
+
+	}
+}
